b02768cd90beb2ff6dd63ce3a5237f78da22084a
Darrin Lowery <ogrotten@monsto.com>
2023-12-22 00:02:07 -0600

    daisyui installed with theme switcher.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	import ThemeSelect from '$lib/components/ThemeSelect.svelte';
+	import { config } from '$lib/config';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+theme chooser
+
+<!-- <select data-choose-theme>
+	{#each config.daisyui.themes as theme}
+		<option value={theme}>{theme}</option>
+	{/each}
+</select> -->
+
+<ThemeSelect />
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
