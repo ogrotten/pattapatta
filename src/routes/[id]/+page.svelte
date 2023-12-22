aa59d61207577f48d6b610b28264dfc6ffb182fa
Darrin Lowery <ogrotten@monsto.com>
2023-12-22 15:54:02 -0600

    session ux

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Container from '../_components/Container.svelte'
+
 	import { fly, fade } from 'svelte/transition'
 	import { onMount } from 'svelte'
 	import { goto } from '$app/navigation'
@@ -60,45 +62,37 @@
 {#if loading}
 	<div class="" />
 {:else}
-	<div class="flex flex-col gap-10">
-		<p class="flex flex-row items-center font-semibold">
-			Pass out this url for the session:
-			<button
-				class="z-50 h-full ml-2 transition-all border w-fit border-warning btn-outline btn-warning btn-xs"
-				on:click={() => {
-					navigator.clipboard.writeText(setupLink).then(() => {})
-					linkcopied = true
-				}}
-			>
-				{setupLink}
-			</button>
-			{#if linkcopied}
-				<button
-					class="z-0 h-full font-bold transition duration-200 border pointer-events-none w-fit border-success btn-outline btn-success btn-xs"
-					transition:fly={{ x: 20 }}
-				>
-					👍 Copied!
-				</button>
-			{/if}
-		</p>
-	</div>
 	<div class="flex justify-end gap-4">
 		<button class="btn btn-primary btn-sm" disabled={sessionActive} on:click={startSession}> Start Session </button>
 		<button class="btn btn-primary btn-sm" disabled={!sessionActive} on:click={endSession}> End Session </button>
 	</div>
 
-	{#if sessionActive}{/if}
-
-	<section class="container">
-		<header class="w-full px-4 py-2 bg-primary text-primary-content">
-			<p class="text-lg font-bold">Session History</p>
-		</header>
-		<main class="m-4">Main content goes here</main>
-		<footer class="flex justify-end px-4 py-2 bg-secondary text-secondary-content">
-			<button class="btn btn-secondary btn-sm">MoreNothing</button>
-			<button class="btn btn-primary btn-sm">Nothing</button>
-		</footer>
-	</section>
+	{#if sessionActive}
+		<Container header>
+			<div class="flex flex-col gap-10" slot="header">
+				<p class="flex flex-row items-center text-sm font-semibold">
+					Pass out this url for the session:
+					<button
+						class="z-50 h-full ml-2 transition-all border w-fit border-warning btn-outline btn-warning btn-xs"
+						on:click={() => {
+							navigator.clipboard.writeText(setupLink).then(() => {})
+							linkcopied = true
+						}}
+					>
+						{setupLink}
+					</button>
+					{#if linkcopied}
+						<button
+							class="z-0 h-full font-bold transition duration-200 border pointer-events-none w-fit border-success btn-outline btn-success btn-xs"
+							transition:fly={{ x: 20 }}
+						>
+							👍 Copied!
+						</button>
+					{/if}
+				</p>
+			</div>
+		</Container>
+	{/if}
 {/if}
 
 <style lang="postcss">
---
+++ src/routes/_components/Container.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let header = false
+	export let footer = false
+</script>
+
+<section class="container">
+	{#if header}
+		<header class="w-full px-4 py-2 bg-primary text-primary-content">
+			<p class="text-lg font-bold"><slot name="header" /></p>
+		</header>
+	{/if}
+	<main class="m-4"><slot name="main" /></main>
+	{#if footer}
+		<footer class="flex justify-end px-4 py-2 bg-secondary text-secondary-content">
+			<slot name="footer" />
+		</footer>
+	{/if}
+</section>
+
+<style lang="postcss">
+	.container {
+		@apply w-full border-2 rounded-lg border-primary my-8;
+	}
+</style>
